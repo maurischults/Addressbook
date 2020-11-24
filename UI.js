e300f1478c32ac8faace6d32888940d613157486
mauris <mauri.schults@khk.ee>
2020-11-24 14:56:50 +0200

    viies commit, objektorienteeritud programmeerimine.

--- UI.js
+++ UI.js
@@ -2,7 +2,7 @@ class UI {
     addPersonToTable(person) {
         // create tr
         const tr = document.createElement('tr');
-        //create all td with person info
+        // create all td with person data
         tr.innerHTML = `<td>${person.firstName}</td>
                         <td>${person.lastName}</td>
                         <td>${person.city}</td>
@@ -12,5 +12,25 @@ class UI {
         `;
         // append tr to table
         contacts.appendChild(tr);
+    }
+
+    alertMessage(message) {
+        // create div for alert message
+        const div = document.createElement('div');
+        // add class to div
+        div.className = "alert";
+        // create string value of message for div
+        const text = document.createTextNode(message);
+        // insert text into div
+        div.appendChild(text);
+        // find components - before and after alert div element
+        const card = document.querySelector('.card-content');
+        const form = document.querySelector('#contact-form');
+        // add alert info html
+        card.insertBefore(div, form);
+        // set alert messega up to 5 sec
+        setTimeout(function () {
+            document.querySelector(".alert").remove();
+        }, 5000);
     }
 }
